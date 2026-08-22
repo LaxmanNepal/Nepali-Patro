5c8337fc96fd66fe7eac5230ce4cdcfbfdf5f2d2
Laxman Nepal <84069546+LaxmanNepal@users.noreply.github.com>
2026-08-23 01:08:00 +0300

    chore: bump cache after homepage runtime fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='nepali-patro-v2026-08-22-01';
+const CACHE_NAME='nepali-patro-v2026-08-23-02';
 const APP_SHELL=['./','./index.html','./manifest.json','./assets/logo.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
