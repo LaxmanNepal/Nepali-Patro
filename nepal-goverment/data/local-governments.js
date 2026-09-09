8ece0e53372835d1e01b0f884e8cc79ecb087d4b
Laxman Nepal <84069546+LaxmanNepal@users.noreply.github.com>
2026-09-10 01:06:51 +0300

    feat(gov): add local government directory dataset foundation

---
+++ nepal-goverment/data/local-governments.js
@@ -0,0 +1,28 @@
+window.GOVERNMENT_LOCAL_META = {
+  source:'https://www.mofaga.gov.np/local-contact/',
+  total:753,
+  note:'स्थानीय तहको आधिकारिक सम्पर्क/वेबसाइट सूची MoFAGA को Local Level Contact बाट विस्तार गर्न सकिने संरचना।'
+};
+
+// Curated verified examples. The directory UI is designed to accept the complete
+// 753-unit dataset without changing the frontend contract.
+window.GOVERNMENT_LOCALS = [
+  {n:'काठमाडौँ महानगरपालिका',e:'Kathmandu Metropolitan City',u:'https://kathmandu.gov.np/',c:'स्थानीय सरकार',l:'local',p:'bagmati',district:'काठमाडौँ',type:'महानगरपालिका',k:'काठमाडौं काठमाडौँ महानगर local government'},
+  {n:'ललितपुर महानगरपालिका',e:'Lalitpur Metropolitan City',u:'https://lalitpurmun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'bagmati',district:'ललितपुर',type:'महानगरपालिका',k:'ललितपुर महानगर local government'},
+  {n:'भरतपुर महानगरपालिका',e:'Bharatpur Metropolitan City',u:'https://bharatpurmun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'bagmati',district:'चितवन',type:'महानगरपालिका',k:'भरतपुर चितवन local government'},
+  {n:'पोखरा महानगरपालिका',e:'Pokhara Metropolitan City',u:'https://pokharamun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'gandaki',district:'कास्की',type:'महानगरपालिका',k:'पोखरा कास्की local government'},
+  {n:'विराटनगर महानगरपालिका',e:'Biratnagar Metropolitan City',u:'https://biratnagarmun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'koshi',district:'मोरङ',type:'महानगरपालिका',k:'विराटनगर मोरङ local government'},
+  {n:'वीरगञ्ज महानगरपालिका',e:'Birgunj Metropolitan City',u:'https://birgunjmun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'madhesh',district:'पर्सा',type:'महानगरपालिका',k:'वीरगञ्ज पर्सा local government'},
+  {n:'नेपालगञ्ज उप-महानगरपालिका',e:'Nepalgunj Sub-Metropolitan City',u:'https://nepalgunjmun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'lumbini',district:'बाँके',type:'उप-महानगरपालिका',k:'नेपालगञ्ज बाँके local government'},
+  {n:'घोडाघोडी नगरपालिका',e:'Ghodaghodi Municipality',u:'https://ghodaghodimun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'sudurpashchim',district:'कैलाली',type:'नगरपालिका',k:'घोडाघोडी कैलाली local government'},
+  {n:'महाबु गाउँपालिका',e:'Mahabu Rural Municipality',u:'https://mahabumun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'karnali',district:'दैलेख',type:'गाउँपालिका',k:'महाबु दैलेख local government'},
+  {n:'गल्याङ नगरपालिका',e:'Galyang Municipality',u:'https://galyangmun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'gandaki',district:'स्याङ्जा',type:'नगरपालिका',k:'गल्याङ स्याङ्जा local government'},
+  {n:'चापाकोट नगरपालिका',e:'Chapakhot Municipality',u:'https://chapakotmun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'gandaki',district:'स्याङ्जा',type:'नगरपालिका',k:'चापाकोट स्याङ्जा local government'},
+  {n:'बबई गाउँपालिका',e:'Babai Rural Municipality',u:'https://babaimun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'lumbini',district:'दाङ',type:'गाउँपालिका',k:'बबई दाङ local government'},
+  {n:'कञ्चनरुप नगरपालिका',e:'Kanchanrup Municipality',u:'https://kanchanrupmun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'madhesh',district:'सप्तरी',type:'नगरपालिका',k:'कञ्चनरुप सप्तरी local government'},
+  {n:'लहान नगरपालिका',e:'Lahan Municipality',u:'https://lahanmun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'madhesh',district:'सिराहा',type:'नगरपालिका',k:'लहान सिराहा local government'},
+  {n:'उत्तरगया गाउँपालिका',e:'Uttargaya Rural Municipality',u:'https://uttargayamun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'bagmati',district:'रसुवा',type:'गाउँपालिका',k:'उत्तरगया रसुवा local government'},
+  {n:'गजुरी गाउँपालिका',e:'Gajuri Rural Municipality',u:'https://gajurimun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'bagmati',district:'धादिङ',type:'गाउँपालिका',k:'गजुरी धादिङ local government'},
+  {n:'खत्याड गाउँपालिका',e:'Khatyad Rural Municipality',u:'https://khatyadmun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'karnali',district:'मुगु',type:'गाउँपालिका',k:'खत्याड मुगु local government'},
+  {n:'नाम्खा गाउँपालिका',e:'Namkha Rural Municipality',u:'https://namkhamun.gov.np/',c:'स्थानीय सरकार',l:'local',p:'karnali',district:'हुम्ला',type:'गाउँपालिका',k:'नाम्खा हुम्ला local government'}
+];
